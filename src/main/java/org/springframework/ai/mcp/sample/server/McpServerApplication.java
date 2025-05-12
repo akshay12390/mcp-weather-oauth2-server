de35fd92c89b3864618a24139c8174d12d5ce8c6
akshay.kumar2 <akshay.kumar2@salesforce.com>
2025-05-12 14:00:01 +0530

    Removing toUpperCase() tool

--- src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
+++ src/main/java/org/springframework/ai/mcp/sample/server/McpServerApplication.java
@@ -17,9 +17,7 @@ package org.springframework.ai.mcp.sample.server;
 
 import java.util.List;
 
-import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.function.FunctionToolCallback;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -54,17 +52,6 @@ public class McpServerApplication {
 	@Bean
 	public ToolCallbackProvider weatherTools(WeatherService weatherService) {
 		return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
-	}
-
-	public record TextInput(String input) {
-	}
-
-	@Bean
-	public ToolCallback toUpperCase() {
-		return FunctionToolCallback.builder("toUpperCase", (TextInput input) -> input.input().toUpperCase())
-			.inputType(TextInput.class)
-			.description("Put the text to upper case")
-			.build();
 	}
 
 	@Bean
